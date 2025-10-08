cded35b01cb608387131331dc1acf684730154af
Teeradet <tee2490@hotmail.com>
2025-10-08 09:51:15 +0700

    Add project files.

---
+++ OOPApp/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace OOPApp
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
+    }
+}
---
+++ OOPApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using OOPApp;
+
+Product product = new Product(); //oop
+Product product1 = new Product();
+
+product.Id = "A001";
+product.Name = "Coffee";
+product.Price = 100.00;
+product.Stock = 10;
+
+Console.WriteLine(product.Id);
+Console.WriteLine(product.Name);
+Console.WriteLine(product.Price);
+Console.WriteLine(product.Stock);
